7c0c1efd1ce72dc4a6214c6ac54d2761ae1a6408
SYED MUHAMMAD SAAD BUKHARI <142715489+saadbukhari00@users.noreply.github.com>
2024-08-11 01:51:55 +0500

    Added Argparse

--- Remote_dll_Injection.py
+++ Remote_dll_Injection.py
@@ -1,10 +1,11 @@
+import argparse
 from ctypes import *
 from ctypes import wintypes
 import subprocess
 
 # Define the data types
 kernel = windll.kernel32
-LPCSTR = c_char_p   
+LPCSTR = c_char_p
 SIZE_T = c_size_t
 
 # Define the functions
@@ -12,13 +13,13 @@ OpenProcess = kernel.OpenProcess
 OpenProcess.argtypes = (wintypes.DWORD, wintypes.BOOL, wintypes.DWORD)
 OpenProcess.restype = wintypes.HANDLE
 
-VirtualAllocEx = kernel.VirtualAllocEx  
+VirtualAllocEx = kernel.VirtualAllocEx
 VirtualAllocEx.argtypes = (wintypes.HANDLE, wintypes.LPVOID, SIZE_T, wintypes.DWORD, wintypes.DWORD)
 VirtualAllocEx.restype = wintypes.LPVOID
 
 WriteProcessMemory = kernel.WriteProcessMemory
 WriteProcessMemory.argtypes = (wintypes.HANDLE, wintypes.LPVOID, wintypes.LPCVOID, SIZE_T, POINTER(SIZE_T))
-WriteProcessMemory.restype = wintypes.BOOL  
+WriteProcessMemory.restype = wintypes.BOOL
 
 GetModuleHandle = kernel.GetModuleHandleA
 GetModuleHandle.argtypes = (LPCSTR,)
@@ -28,12 +29,12 @@ GetProcAddress = kernel.GetProcAddress
 GetProcAddress.argtypes = (wintypes.HANDLE, LPCSTR)
 GetProcAddress.restype = wintypes.LPVOID
 
-# Define the structure as we need to pass the structure as an argument   
+# Define the structure as we need to pass the structure as an argument
 class _Security_Attributes(Structure):
     _fields_ = [("nLength", wintypes.DWORD),
                 ("lpSecurityDescriptor", wintypes.LPVOID),
-                ("bInheritHandle", wintypes.BOOL),]  
-    
+                ("bInheritHandle", wintypes.BOOL),]
+
 security_attributes = _Security_Attributes
 lpsecurity_attributes = POINTER(security_attributes)
 lpthread_start_routine = wintypes.LPVOID
@@ -45,12 +46,17 @@ CreateRemoteThread.restype = wintypes.HANDLE
 # We need some constants to work with memory
 MEM_COMMIT = 0x00001000
 MEM_RESERVE = 0x00002000
-PAGE_READWRITE = 0x04       
+PAGE_READWRITE = 0x04
 EXECUTE_IMMEDIATELY = 0x0
 PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0x00000FFF)
 
-# Path to the DLL to inject
-dll_path = b"fun.dll"
+# Set up argparse for command line arguments
+parser = argparse.ArgumentParser(description='Inject a DLL into a process.')
+parser.add_argument('dll_path', type=str, help='Path to the DLL file to inject')
+args = parser.parse_args()
+
+# Convert DLL path to bytes
+dll_path = args.dll_path.encode()
 
 # Start a new process (e.g., notepad.exe)
 process = subprocess.Popen(["notepad.exe"])
@@ -63,14 +69,14 @@ if not handle:
     print("Could not open the process")
     exit(1)
 
-print("Handle Obtained")   
+print("Handle Obtained")
 
 # Allocate memory in the remote process
 address = VirtualAllocEx(handle, None, len(dll_path) + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE)
 
 if not address:
     print("Could not allocate memory in the remote process")
-    exit(1)     
+    exit(1)
 
 print("Memory Allocated")
 
@@ -91,7 +97,7 @@ if not load_lib:
     print("Could not get the address of LoadLibraryA")
     exit(1)
 
-# Start the remote thread 
+# Start the remote thread
 remotethread = CreateRemoteThread(handle, None, 0, load_lib, address, EXECUTE_IMMEDIATELY, None)
 
 if not remotethread:
